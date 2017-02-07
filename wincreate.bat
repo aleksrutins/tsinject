392120d45d2eea55c01c0320666dd8d8bea44e20
munchkinhalfling <alex@rutins.com>
2017-02-07 16:51:33 -0500

    Create the creator

---
+++ wincreate.bat
@@ -0,0 +1,14 @@
+@echo off
+mkdir $0
+echo "<!doctype html>
+<html>
+  <head>
+    <title>$0</title>
+    <script src='tsinject.js'></script>
+  </head>
+  <body>
+    <script>
+      // your tsinject code here!
+    </script>
+  </body>
+</html>">"./$0/index.html"
