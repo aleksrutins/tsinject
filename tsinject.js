8a026ec9331b63ba58c695e250b229f1fbde8308
munchkinhalfling <munchkin@rutins.com>
2017-02-07 16:17:08 -0500

    More stuff explained. Also added $doc.onReady() function.

--- tsinject.js
+++ tsinject.js
@@ -11,29 +11,47 @@ function getParamNames(func) {
   return result;
 }
 var tsinject = {
+    /**
+     * "injectables": dependencies that can be injected.
+     * For internal use only.
+     */
     injectable: {
         $doc: {
             /**
              * @param {...string} selectors
              * @returns {Element}
+             * alias for document.querySelector(...selectors)
              */
             select(...selectors) {
                 return document.querySelector(selectors);
             },
             /**
              * @param {string} elType
+             * Alias for document.createElement(elType)
              */
             createEl(elType) {
                 return document.createElement(elType);
             },
             base: document,
+            /**
+             * Alias for document.body
+             */
             getBody() {
                 return document.body;
+            },
+            /**
+             * Alias for 'window.onload = f;'
+             */
+            onReady(f) {
+                window.onload = f;
             }
         }
     },
     /**
      * @param {function()} f
+     * resolves the dependencies of f (a.k.a @inject <dependency> directives). All dependencies must be names of injectables.
+     * Default injectables:
+     *  $doc: Helper functions for the document objects. Also includes a "base" property assigned to the document.
      */
     resolve: function(f) {
         let strRep = f.toString();
@@ -52,11 +70,22 @@ var tsinject = {
         strRep = lines.join('\n');
         return eval(strRep);
     },
+    /**
+     * Registers a new injectable.
+     * @param {string} name
+     * @param {any} value
+     */
     register: function(name, value) {
         tsinject.injectable[name] = value;
     },
+    /**
+     * Binds a function to the context of ctx.
+     */
     preparef: function(obj, ctx) {
         return obj.bind(ctx);
     },
+    /**
+     * Another way of commenting things out.
+     */
     ignore: function(code) {}
 }
